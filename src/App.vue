e0db17563749a043ecfa5a4733670c4e6fe1cba5
ekoeryanto <ekoeryanto@gmail.com>
2020-11-05 14:52:54 +0700

    chore: step2

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,32 @@
 <template>
-  <div id="landing">
-    <div class="landing-page">
-      
-      <div class="alpha"></div>
-      <div class="beta"></div>
-
-
-      <div class="heading">
-        <h1>{{ title }}</h1>
-        <h2>{{ instance }}</h2>
-      </div>
-
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
-      <br>
-      been the industry's standard dummy
-    </p>
-
-     <div class="buttons">
-      <b-button size="is-medium" class="is-success"
-        icon-left="phone">
-        Login dengan telepon
-      </b-button>
-
-      <b-button size="is-medium"
-        icon-left="google">
-        Login dengan Google
-      </b-button>
-
-    </div>
-    </div>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-  data() {
-    return {
-      title: 'Survey Minat Pelanggan',
-      instance: 'PDAM Tirta Patriot Kota Bekasi.'
-    }
-  }
-}
-</script>
-
 <style>
-.heading h1 {
-  font-weight: 800;
-  font-size: 20px;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-.heading h2 {
-  font-weight: 800;
-  font-size: 15px;
+#nav {
+  padding: 30px;
 }
 
-.landing-page {
-  margin-top: 230px;
-  margin-left: 100px;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-p {
-  padding-top: 20px;
-  padding-bottom: 20px;
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
-
-.alpha, .beta {
-  height: 500px;
-  width: 300px;
-  border-radius: 10%;
-  transform: rotate(50deg)
-}
-
-.alpha {
-  position: fixed;
-  top: -300px;
-  right: 30%;
-  opacity: 0.7;
-  background-color: skyblue;
-}
-
-.beta {
-  position: fixed;
-  top: -300px;
-  right: 20%;
-  opacity: 0.2;
-
-  background-color: skyblue;
-}
-
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="landing">
+    <div class="landing-page">
+      
+      <div class="alpha"></div>
+      <div class="beta"></div>
+
+
+      <div class="heading">
+        <h1>{{ title }}</h1>
+        <h2>{{ instance }}</h2>
+      </div>
+
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
+      <br>
+      been the industry's standard dummy
+    </p>
+
+     <div class="buttons">
+      <b-button size="is-medium" class="is-success"
+        icon-left="phone">
+        Login dengan telepon
+      </b-button>
+
+      <b-button size="is-medium" icon-left="google" @click="login">
+        Login dengan Google
+      </b-button>
+
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'Survey Minat Pelanggan',
+      instance: 'PDAM Tirta Patriot Kota Bekasi.'
+    }
+  },
+  methods: {
+    login() {
+      this.$router.push('/about')
+    }
+  }
+}
+</script>
+
+<style>
+.heading h1 {
+  font-weight: 800;
+  font-size: 20px;
+}
+.heading h2 {
+  font-weight: 800;
+  font-size: 15px;
+}
+.landing-page {
+  margin-top: 230px;
+  margin-left: 100px;
+}
+p {
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+.alpha, .beta {
+  height: 500px;
+  width: 300px;
+  border-radius: 10%;
+  transform: rotate(50deg)
+}
+.alpha {
+  position: fixed;
+  top: -300px;
+  right: 30%;
+  opacity: 0.7;
+  background-color: skyblue;
+}
+.beta {
+  position: fixed;
+  top: -300px;
+  right: 20%;
+  opacity: 0.2;
+  background-color: skyblue;
+}
+</style>
