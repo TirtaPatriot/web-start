5ed8f4525caf6a1e0a453426ec0cbf028a3e9599
ekoeryanto <ekoeryanto@gmail.com>
2020-11-10 10:57:43 +0700

    chore: change to bootstrap

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
 import router from './router'
 
-Vue.config.productionTip = false
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(Buefy)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
